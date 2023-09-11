25e3bf1fd082cdf8a00893bb7b3bf46e620e9e65
Rodrigo Torres de Souza <rtsouza87@gmail.com>
2023-09-11 14:07:54 -0300

    Add files via upload

---
+++ Statistic/anova_python.py
@@ -0,0 +1,25 @@
+#Teste.csv - completo | Teste2.csv - soma dos registros duplicados + | Teste3.csv - Soma acima de 1000
+
+import pandas as pd
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+from statsmodels.stats.anova import anova_lm
+
+# dataAnova = pd.read_csv('Teste.csv', encoding='ISO-8859-1')
+dataAnova = pd.read_csv('Teste5.csv')
+
+
+# print(dataAnova.head())
+
+# modelo = ols('QDE_VENDIDA ~ DESCRICAO * COR * TECIDO', data=dataAnova).fit()
+
+
+# Para arquivo Teste4.csv
+# modelo = ols('QDE_VENDIDA ~ COR * TECIDO * BASE', data=dataAnova).fit()
+
+
+modelo = ols('QDE_VENDIDA ~ TECIDO * COR', data=dataAnova).fit()
+
+tabela_anova = anova_lm(modelo)
+
+print(tabela_anova)
